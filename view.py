dd55e72537253b179d1058d224df324155153653
Dmitriy Filatov <filatov-d@mail.ru>
2022-12-16 23:08:43 +0300

    Function 'print_data' with lib 'tabulate' was added

--- view.py
+++ view.py
@@ -1,27 +1,33 @@
 from logger import log
-
+import var
+from tabulate import tabulate # для установки библиотеки: pip install tabulate  
 
 @log
 def greetings():
     '''Вывод приветствия.'''
 
-    print('Справочник')
+    print('\n**Справочник студентов ВУЗА**')
 
 
 @log
-def menu() -> str:
+def menu():
     ''' редактировать добавлять, удалять
     Вывод меню, редактирование, добавление, удаление данных.
     :return:
     '''
 
-    print('Меню')
+    print('\nМеню:\n')
     print('0 - Выход \n'
           '1 - Загрузить из файла и вывести на экран \n'
           '2 - Добавить новую запись \n'
           '3 - Редактировать запись по id \n'
           '4 - Удаление записи \n')
 
-
-def print_data():
-    pass
+@log
+def print_data(data: list):
+    """
+    Вывод в консоль данных содержимого справочника
+    """
+    print(tabulate(data, headers = var.field, tablefmt="grid"))
+    
+    
