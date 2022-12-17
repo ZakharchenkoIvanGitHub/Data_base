0b83e02b235ee5044974d2f3b8e05f2c6668a146
Vera <tarasevich28_88@mail.ru>
2022-12-17 16:31:38 +0300

    work

--- request.py
+++ request.py
@@ -1,6 +1,22 @@
 def get_command():
-    return input()
+    return input('Введите пункт меню: ')
 
+def get_id():
+    return input('Введите id: ')
 
 def get_data():
-    return input("Фамилия"), input("имя"), input("Дата рождения")
+    return input("Фамилия: "), input("Имя: "), input("Дата рождения: ")
+
+
+
+# def add_contact():
+#     data = []
+#     for col in header:
+#         data.append(input(f'Введите {col}: '))
+#     return data
+
+# header = ['фамилия: ', 'имя: ', 'день рождения: ']
+
+if __name__ == '__main__':
+    pass
+
